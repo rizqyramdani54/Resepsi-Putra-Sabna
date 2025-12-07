9ada5af283f0d65ea2c4785a66a0c96ac0e66888
rizqyramdani54 <rizqyramdani54@gmail.com>
2025-12-07 10:01:07 +0700

    modified:   index.html 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const openButton = document.getElementById('openButton');
+    const coverSection = document.getElementById('coverSection');
+    const mainContent = document.getElementById('mainContent');
+    const audio = document.getElementById('backgroundMusic');
+    const body = document.body;
+    openButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const playPromise = audio.play();
+        if (playPromise !== undefined) {
+            playPromise.then(() => {
+                console.log('Musik berhasil diputar!');
+            }).catch(error => {
+                console.error('Gagal memutar musik setelah interaksi:', error);
+            });
+        }
+        body.classList.remove('no-scroll');
+        coverSection.style.display = 'none';
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
+
+const Hari = document.getElementById('hari');
+const Jam = document.getElementById('jam');
+const Menit = document.getElementById('menit');
+
+const targerTanggal = new Date("January 11 2026 00:00:00").getTime();
+
+function timer() {
+    const tanggalsaatini = new Date().getTime();
+    const jarak = targerTanggal - tanggalsaatini;
+
+    const hari = Math.floor(jarak / 1000 / 60 / 60 / 24);
+    const jam = Math.floor(jarak / 1000 / 60 / 60) % 24;
+    const menit = Math.floor(jarak / 1000 / 60) % 60;
+
+    Hari.innerHTML = hari;
+    Jam.innerHTML = jam;
+    Menit.innerHTML = menit;
+}
+
+setInterval(timer, 1000);
