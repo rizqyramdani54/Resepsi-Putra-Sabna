03389593421e411eda65588100c11f53109ed1ca
Muhammad Rizqy Ramdani <rizqyramdani54@gmail.com>
2025-12-25 12:00:04 +0700

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -40,3 +40,19 @@ function timer() {
 }
 
 setInterval(timer, 1000);
+
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Konfirmasi kehadiran berhasil terkirim!");
+    })
+  });
+});
