ca4e63229ebe98502b7cf103874ba442a00b76cf
Muhammad Rizqy Ramdani <rizqyramdani54@gmail.com>
2025-12-25 12:02:43 +0700

    modified:   script.js

--- script.js
+++ script.js
@@ -41,18 +41,18 @@ function timer() {
 
 setInterval(timer, 1000);
 
-window.addEventListener("load", function() {
-  const form = document.getElementById('my-form');
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    const data = new FormData(form);
-    const action = e.target.action;
-    fetch(action, {
-      method: 'POST',
-      body: data,
-    })
-    .then(() => {
-      alert("Konfirmasi kehadiran berhasil terkirim!");
-    })
-  });
+window.addEventListener("load", function () {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const data = new FormData(form);
+        const action = e.target.action;
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        })
+            .then(() => {
+                alert("Konfirmasi kehadiran berhasil terkirim!");
+            })
+    });
 });
